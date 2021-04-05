f9ce96f74cfe7ab84de8861f47dac2cf33e51d5f
손지수 <sonjisuu@naver.com>
2021-04-05 11:29:54 +0900

    111

---
+++ 01-basic.js
@@ -0,0 +1,11 @@
+//  한 줄 주석
+/*
+    여러 줄 주석: C, Java등과 유사
+*/ 
+//  내장객체 console : 콘솔에 출력하는 객체
+console.info("정보 메세지 출력");
+console.debug("디버그 메세지 출력");
+console.warn("경고 메세지");
+console.error("에러 메세지");
+
+console.log("가장 일반적인 로그 메세지");
